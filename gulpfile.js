c7152e4f129dd56e04f408f16bbf91dd0abc696a
DayDay <daydaybigking@sina.com>
2020-10-12 16:51:10 +0800

    完成转换工具初步搭建

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const { src, dest } = require("gulp");
+const imagemin = require("gulp-imagemin");
+
+module.exports = function () {
+  return src(["imageSrc/*", "imageSrc/**/*"])
+    .pipe(
+      imagemin([
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.mozjpeg({ quality: 75, progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 }),
+        imagemin.svgo({
+          plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
+        }),
+      ])
+    )
+    .pipe(dest("imageBuild/"));
+};
